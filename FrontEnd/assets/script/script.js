a4dadeee0e52db5e973cc105f7d6e53448e62017
Baptiste Giroud <giroudbpro@gmail.com>
2023-11-29 20:01:57 +0100

    Récupération et affichage de la galerie API

--- FrontEnd/assets/script/script.js
+++ FrontEnd/assets/script/script.js
@@ -1,2 +1,22 @@
-// Filters // 
+// Get projects // 
+
+let gallery = document.querySelector(".gallery")
+
+function cleansPortfolio(){
+    gallery.innerHTML=""
+}
+
+async function getProjects(){
+    let works = await fetch("http://localhost:5678/api/works").then(res =>
+    res.json())
+    console.log(works)
+    for (i=0; i < works.length; i++){
+        let imageUrl = works[i].imageUrl
+        let title = works[i].title
+        gallery.insertAdjacentHTML("afterbegin", `<figure><img src="${imageUrl}" alt="${title}"><figcaption>${title}</figcaption></figure>`)
+    }
+}
+
+cleansPortfolio()
+getProjects()
 
