ec5fb316aba1c977897e500af939a941da128a3a
Baptiste Giroud <giroudbpro@gmail.com>
2023-12-18 12:08:38 +0100

    Rework JS - Modal fonctionnel - Removed Project

--- FrontEnd/assets/script/login.js
+++ FrontEnd/assets/script/login.js
@@ -5,8 +5,6 @@ let loginButton = document.querySelector(" .btn")
 let passwordInput = document.getElementById("password")
 let emailInput = document.getElementById("email")
 
-let token = window.localStorage.getItem("token")
-
 
 // Login request //
 
@@ -18,21 +16,24 @@ async function loginRequest(){
     let chargeUtile = JSON.stringify(loginForm)
 
 
-    let responseAjax =  await fetch("http://localhost:5678/api/users/login", {
+    return await fetch("http://localhost:5678/api/users/login", {
         method: "POST",
         headers: { "Content-Type": "application/json"},
         body: chargeUtile,
     }).then(resp => resp.json())
-    .then((user) => {window.localStorage.setItem("token", user.token)})
-    .then(token = window.localStorage.getItem("token"))
-
-    document.location = "./index.html"
 }
 
 /** Main **/
 
 loginButton.addEventListener("click", (event) => {
     event.preventDefault()
-    loginRequest()
+    loginRequest().then((response) => {
+        if (response.error || response.message){
+            alert("Login failed")
+        } else {
+            window.localStorage.setItem("token", response.token)
+            location = "./index.html"
+        }
+    })
 })
 
--- FrontEnd/assets/script/script.js
+++ FrontEnd/assets/script/script.js
@@ -1,41 +1,62 @@
 // Get projects // 
 
 let gallery = document.querySelector(".gallery")
-let buttonsFilters = document.querySelectorAll(".filters input")
+let galleryModal = document.querySelector(".grid")
+let filters = document.querySelector(".filters")
 
-function cleansPortfolio(){
-    gallery.innerHTML=""
-}
-
-function displayPortfolio(works){
-    for (i = 0; i < works.length; i++){
-        let imageUrl = works[i].imageUrl
-        let title = works[i].title
-        gallery.insertAdjacentHTML("beforeend", `
-        <figure>
-        <img src="${imageUrl}" alt="${title}">
-        <figcaption>${title}</figcaption>
-        </figure>`)
+function cleansPortfolio( isEdit = false){
+    if (isEdit){
+        galleryModal.innerHTML = ""
+    } else {
+        gallery.innerHTML=""
     }
 }
 
-async function getProjects(){
-    let works = await fetch("http://localhost:5678/api/works").then(res =>
-    res.json())
-    displayPortfolio(works)
+function displayFilters(categories){
+    for (i = 0; i < categories.length; i++){
+        let categoryId = categories[i].id
+        let categoryName = categories[i].name
 
-    buttonsFilters.forEach((buttonsFilters) => {
-            buttonsFilters.addEventListener("click", (event) => {
-                let filters = works.filter(function (works){
-                    return event.currentTarget.dataset.category == parseInt(works.category.id);
-                })
-                if (event.currentTarget.dataset.category == "null"){
-                    location.reload()
-                }
-            cleansPortfolio()
-            displayPortfolio(filters)
-        })
-    })
+        filters.insertAdjacentHTML("beforeend", `
+        <div class="filters_btn">
+		<input type="radio" name="filter" data-category="${categoryId}" id="${categoryName}">
+		<label for="${categoryName}">${categoryName}</label>
+		</div>`)
+    }
+}
+
+function displayPortfolio(works, isEdit = false){
+    for (i = 0; i < works.length; i++){
+        let imageUrl = works[i].imageUrl
+        let title = works[i].title
+        let workId = works[i].id
+        
+        if (isEdit){
+            galleryModal.insertAdjacentHTML("beforeend", `
+            <figure data-id="${workId}">
+            <i class="fa-solid fa-trash-can fa-xs removedProject"></i>
+            <img src="${imageUrl}" alt="${title}">
+            </figure>`)
+        
+        } else {
+            gallery.insertAdjacentHTML("beforeend", `
+            <figure>
+            <img src="${imageUrl}" alt="${title}">
+            <figcaption>${title}</figcaption>
+            </figure>`)
+        }
+    }
+    // removedProject()
+}
+
+async function getProjects(){
+    return await fetch("http://localhost:5678/api/works").then(res =>
+    res.json())
+}
+
+async function getCategories(){
+    return await fetch("http://localhost:5678/api/categories").then(res =>
+    res.json())
 }
 
 // Edition mode //
@@ -50,8 +71,11 @@ let modalContent = document.querySelector(".content_gallery")
 let btnEdition = document.querySelector(".modify")
 let markEdition = document.querySelector(".fa-xmark")
 
+let logInOut = document.getElementById("logInOut")
+let header = document.querySelector("header")
 
-if ((token == "undefined") || (localStorage == 0)){
+
+if (token == null){
     console.error("Login failed")
     for (i = 0; i < elementsEdit.length; i++){
         elementsEdit[i].classList.add("hidden")
@@ -63,29 +87,150 @@ if ((token == "undefined") || (localStorage == 0)){
         elementsEdit[i].classList.remove("hidden")
         modal.classList.add("hidden")
     }
+    header.classList.add("headerEdition")
+    logInOut.innerHTML = `<li id="logInOut"><a href="./login.html">logout</a></li>`
 }
 
 // Open close Modal //
 
 btnEdition.addEventListener("click", (event) => {
-    modal.classList.remove("hidden")
+    modal.classList.remove("hidden","modalDisappear")
+    modalModule.classList.remove("modalSlideOff")
+    getProjects().then((works) => {
+        cleansPortfolio(true)
+        displayPortfolio(works, true)
+    })
 })
 
 markEdition.addEventListener("click", (event) => {
-    modal.classList.add("hidden")
+    modalModule.classList.add("modalSlideOff")
+    modal.classList.add("modalDisappear")
+    window.setTimeout(function (){
+        modal.classList.add("hidden")
+        /* Retour first Modal */
+        modalContent.classList.remove("hidden")
+        content_addWork.classList.add("hidden")
+        returnModalGallery.classList.add("masked") 
+    }, 300)   
 })
 
 document.addEventListener("click", (event) => {
     if (event.target == modal) {
-        modal.classList.add("hidden")
+        modalModule.classList.add("modalSlideOff")
+        modal.classList.add("modalDisappear")
+        window.setTimeout(function (){
+            modal.classList.add("hidden")
+            /* Retour first Modal */
+            modalContent.classList.remove("hidden")
+            content_addWork.classList.add("hidden")
+            returnModalGallery.classList.add("masked") 
+        }, 300)      
     }
 })
+
+// Removed Project //
+
+function removedProject() {
+    let buttonSup = document.querySelectorAll(".removedProject")
+    buttonSup.forEach((buttonSup) => {
+        buttonSup.addEventListener("click", () =>{
+            let selectProjectId = buttonSup.parentElement.dataset.id
+            let requestRemoved = fetch(`http://localhost:5678/api/works/${selectProjectId}`, {
+                method: "DELETE",
+                headers: {Authorization: `Bearer ${token}`},
+            })
+        })
+    })
+}
+
+// Add Project //
+
+let buttonAddPhoto = document.getElementById("add_photo")
+let content_addWork = document.querySelector(".content_addWork")
+let returnModalGallery = document.querySelector(".fa-arrow-left")
+let selectCategories = document.getElementById("selectCategory")
+
+
+function addProject(categories) {
+    buttonAddPhoto.addEventListener("click", (event)=> {
+        modalContent.classList.add("hidden")
+        content_addWork.classList.remove("hidden")
+        returnModalGallery.classList.remove("masked")
+    })
+    returnModalGallery.addEventListener("click", ()=> {
+        modalContent.classList.remove("hidden")
+        content_addWork.classList.add("hidden")
+        returnModalGallery.classList.add("masked")        
+    })
+
+
+    /* Ajout Options SelectBar Category */
+    for (i = 0; i < categories.length; i++){
+    selectCategories.insertAdjacentHTML("beforeend", `
+    <option value="${categories[i].name}">${categories[i].name}</option>`)
+    }
+
+    /* Formulaire AddProject */
+    let formDataAddPhoto = new FormData()
+    let imgTitle = document.getElementById("imgtitle")
+    let imgCategory = document.getElementById("selectCategory")
+    let imgFile = document.getElementById("imgfile")
+    let btnValid = document.getElementById("send_request")
+
+    formDataAddPhoto.append("title", `${imgTitle.value}`)
+    formDataAddPhoto.append("category", `${imgCategory.value}`)
+    formDataAddPhoto.append("image", `${imgFile}.files`)
+
+
+    btnValid.addEventListener("click", () => {
+        if (imgFile.files.length == 0){
+            imgFile.classList.add("errorImg")
+        } else {
+            imgFile.classList.remove("errorImg")  
+        }
+        if (imgTitle.value == ""){
+            imgTitle.classList.add("errorTitle")
+        } else {
+            imgTitle.classList.remove("errorTitle")
+        }
+        if (imgFile.files.length != 0 && imgTitle.value != ""){
+            console.log(imgFile.files)
+            console.log(imgTitle.value)
+            console.log(imgCategory.value)
+            console.log(token)
+        }
+    })
+}
 
 
 // MAIN // 
 
 cleansPortfolio()
-getProjects()
+
+getProjects().then((works) => {
+    displayPortfolio(works)
+
+    getCategories().then((categories) => {
+        displayFilters(categories)
+        addProject(categories)
+        let buttonsFilters = document.querySelectorAll(".filters input")
+
+        buttonsFilters.forEach((buttonsFilters) => {
+            buttonsFilters.addEventListener("click", (event) => {
+                let filterWorks = works
+                let currentFilter = event.currentTarget
+
+                if (currentFilter.dataset.category !== "null") {
+                    filterWorks = works.filter(function (works){
+                        return currentFilter.dataset.category == parseInt(works.category.id);
+                    })
+                }
+                cleansPortfolio()
+                displayPortfolio(filterWorks)
+            })
+        })        
+    })
+})
 
 
 
